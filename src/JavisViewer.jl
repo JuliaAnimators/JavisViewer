8a807628f5edf70a4351b1350c838d10090ad3b2
gpucce <g.puccetti92@gmail.com>
2022-01-22 13:22:26 +0100

    move functionality

--- src/JavisViewer.jl
+++ src/JavisViewer.jl
@@ -5,7 +5,43 @@ using Gtk
 using GtkReactive
 using Javis
 
+include("javis_viewer.jl")
 
+export stream
+export setup_stream, cancel_stream
 
+function stream(
+    video::Video;
+    framerate = 30,
+    pathname = "javis_$(randstring(7)).gif",
+    liveview = false,
+    streamconfig::Union{StreamConfig,Nothing} = nothing,
+    tempdirectory = "",
+    ffmpeg_loglevel = "panic",
+    rescale_factor = 1.0,
+    postprocess_frames_flow = identity,
+    postprocess_frame = Javis.default_postprocess,
+)
+
+    v = render(
+        video::Video;
+        framerate = framerate,
+        pathname = pathname,
+        liveview = liveview,
+        tempdirectory = tempdirectory,
+        ffmpeg_loglevel = ffmpeg_loglevel,
+        rescale_factor = rescale_factor,
+        postprocess_frames_flow = postprocess_frames_flow,
+        postprocess_frame = postprocess_frame,
+    )
+
+    # check if livestream is used and livestream if that's the case
+    if liveview
+        _javis_viewer(v...)
+        return "Live Preview Started"
+    else
+        _livestream(streamconfig, framerate, video.width, video.height, pathname)
+    end
+end
 
 end
---
+++ test/runtests.jl
@@ -0,0 +1,42 @@
+using Animations
+using GtkReactive
+using Gtk: get_gtk_property, visible
+using Images
+import Interact
+import Interact: @map, Widget, Widgets, @layout!, hbox, vbox
+using Javis
+import Latexify: latexify
+using LaTeXStrings
+using ReferenceTests
+using Test
+using VideoIO
+using JavisViewer
+
+function ground(video, action, framenumber)
+    background("white")
+    sethue("blue")
+    return framenumber
+end
+
+function ground_black_on_white(video, action, framenumber)
+    background("white")
+    sethue("black")
+    return framenumber
+end
+
+function ground_color(color_bg, color_pen, framenumber)
+    background(color_bg)
+    sethue(color_pen)
+    return framenumber
+end
+
+function circle_with_color(position, radius, action, color)
+    sethue(color)
+    circle(position, radius, action)
+end
+
+@testset "JavisViewer" begin
+    @testset "Javis Viewer" begin
+        include("viewer.jl")
+    end
+end
