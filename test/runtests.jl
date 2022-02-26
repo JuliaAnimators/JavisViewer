1ea42ba52050868df35f89b7262df5ea0f5ff4ec
gpucce <g.puccetti92@gmail.com>
2022-02-26 23:48:09 +0100

    this should do it

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,31 +12,88 @@ using Test
 using VideoIO
 using JavisViewer
 
-function ground(video, action, framenumber)
-    background("white")
-    sethue("blue")
-    return framenumber
-end
-
-function ground_black_on_white(video, action, framenumber)
+function ground(args...)
     background("white")
     sethue("black")
-    return framenumber
 end
 
-function ground_color(color_bg, color_pen, framenumber)
-    background(color_bg)
-    sethue(color_pen)
-    return framenumber
-end
+@testset "Javis Viewer" begin
+    astar(args...; do_action = :stroke) = star(O, 50, 5, 0.5, 0, do_action)
+    acirc(args...; do_action = :stroke) = circle(Point(100, 100), 50, do_action)
 
-function circle_with_color(position, radius, action, color)
-    sethue(color)
-    circle(position, radius, action)
-end
+    vid = Video(500, 500)
+    back = Background(1:100, ground)
+    star_obj = Object(1:100, astar)
+    act!(star_obj, Action(morph_to(acirc; do_action = :fill)))
 
-@testset "JavisViewer" begin
-    @testset "Javis Viewer" begin
-        include("viewer.jl")
+    l1 = @JLayer 20:60 100 100 Point(0, 0) begin
+        obj = Object((args...) -> circle(O, 25, :fill))
+        act!(obj, Action(1:20, appear(:fade)))
     end
+
+    liveview(vid; pathname = "")
+
+    action_list = [back, star_obj]
+
+    viewer_win, frame_dims, r_slide, tbox, canvas, actions, total_frames, video =
+        JavisViewer._javis_viewer(vid, 100, action_list, false)
+    visible(viewer_win, false)
+
+    @test get_gtk_property(viewer_win, :title, String) == "Javis Viewer"
+
+    JavisViewer._increment(
+        video,
+        [r_slide, tbox],
+        actions,
+        frame_dims,
+        canvas,
+        total_frames,
+    )
+    sleep(0.1)
+    curr_frame = Reactive.value(r_slide)
+    second_frame = Javis.get_javis_frame(video, actions, curr_frame, layers = [l1])
+    @test Reactive.value(r_slide) == 2
+
+    JavisViewer._decrement(
+        video,
+        [r_slide, tbox],
+        actions,
+        frame_dims,
+        canvas,
+        total_frames,
+    )
+    sleep(0.1)
+    curr_frame = Reactive.value(r_slide)
+    first_frame = Javis.get_javis_frame(video, actions, curr_frame, layers = [l1])
+    @test Reactive.value(r_slide) == 1
+
+    @test first_frame != second_frame
+
+    JavisViewer._decrement(
+        video,
+        [r_slide, tbox],
+        actions,
+        frame_dims,
+        canvas,
+        total_frames,
+    )
+    sleep(0.1)
+    curr_frame = Reactive.value(r_slide)
+    last_frame = Javis.get_javis_frame(video, actions, curr_frame, layers = [l1])
+    @test curr_frame == total_frames
+
+    JavisViewer._increment(
+        video,
+        [r_slide, tbox],
+        actions,
+        frame_dims,
+        canvas,
+        total_frames,
+    )
+    sleep(0.1)
+    curr_frame = Reactive.value(r_slide)
+    first_frame = Javis.get_javis_frame(video, actions, curr_frame, layers = [l1])
+    @test curr_frame == 1
+
+    @test last_frame != first_frame
 end
